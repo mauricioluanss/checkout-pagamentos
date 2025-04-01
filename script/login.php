41791e12e6c04f2c8899f97cd5a0e2a2ada03a5e
Mauricio Luan <mauricioluan2023@gmail.com>
2025-04-01 17:54:11 -0300

    - retorno da senha temporária para a pagina de login. - retorno de erro quando inserida credenciais incorretas. - refatoração do código.

--- script/login.php
+++ script/login.php
@@ -1,25 +1,25 @@
 <?php
-//chama o arquivo de conexão com o banco de dados
+/**
+ * Script validar credenciais de login do usuário no banco de dados.
+ */
 require_once('config.php');
 
-//recebe o email e a senha do formulario em login.html
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-//faz o select no banco, passando como parâmetro os campos email e senha recebidos do formulario
 $resultado = $conexao->query("SELECT * FROM usuarios WHERE email='$email' AND senha='$password'");
 
-// valida se o select realizado acima deu resultado, ou seja, se existe alguma linha no banco que
-// corresponda as credencias enviadas no formulario. Se sim, inicia uma sessao php e direciona pra pagina do
-// caixa.
 if ($resultado->num_rows > 0) {
     session_start();
     $_SESSION['logado'] = true;
     header('Location: ../static/tela_operador.html');
     exit();
 } else {
-    header('Location: login.php?erro=1');
-
+    //header('Location: login.php?erro=1');
+    echo "<script>
+            alert('Email ou senha incorretos!');
+            window.location.href = '../static/login.html';
+          </script>";
 }
 
 $conexao->close();
--- script/recuperar_senha.php
+++ script/recuperar_senha.php
@@ -1,21 +1,35 @@
 <?php
-// Script para recuperar a senha do usuário. 
-
- // isso aqui importa o arquivo de configuração do banco de dados, onde é estabelecida a conexão.
+/**
+ * Script pra recuperação de senha do usuário.
+ */
 require_once('config.php');
 
-// recebe o email do usuário através do formulario html.
-// depois faz a consulta no banco e valida se o email existe.
+class senha {
+    public $senha;
+    public function __construct($senhaTemporaria) {
+        $this->senha = $senhaTemporaria;
+    }
+    public function getSenha() {
+        return $this->senha;
+    }
+}
+
 $email = $_POST['email'];
 $verificacao = $conexao->query("SELECT * FROM usuarios WHERE email='$email'");
 
-// se existir registro com o email, gera uma senha temporária e atualiza no banco de dados.
 if ($verificacao->num_rows > 0) {
-    $senha_temporaria = random_int(100000, 9999999);
-    $insert_senha_temporaria = $conexao->query("UPDATE usuarios SET senha='$senha_temporaria' WHERE email='$email'");
-    echo "Nova senha gerada: " . $senha_temporaria;
-    /*
-    aqui vou adicionar depois o código para enviar a senha temporária para o email do usuário.
-    */
+    gerarSenhaAleatoria($conexao, $email);
 }
+function gerarSenhaAleatoria($conexao, $email) {
+    $senha = new senha(random_int(100000, 9999999));
+    $senhaString = $senha->getSenha();
+    $conexao->query("UPDATE usuarios SET senha='$senhaString' WHERE email='$email'");
+
+    echo "
+    <script>
+        alert('Sua senha temporária é: $senhaString');
+        window.location.href = '../static/login.html';
+    </script>";
+}
+$conexao->close();
 ?>
