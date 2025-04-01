8731d0e655f19a5d54af1def5e625bd4c9f9e3ab
Mauricio Luan <mauricioluan2023@gmail.com>
2025-04-01 13:52:25 -0300

    atulizacao

--- db/tb_produtos.sql
+++ db/tb_produtos.sql
@@ -4,6 +4,5 @@ CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     produto VARCHAR(100) NOT NULL UNIQUE,
     quantidade INT NOT NULL,
-    preco INT NOT NULL,
-    --data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP
+    preco DECIMAL(10,2) NOT NULL
 );
